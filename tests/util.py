97f7c82185900ae76923248a4b9bc52d8f8d1e1a
Glinte <96855131+Glinte@users.noreply.github.com>
2024-11-18 13:17:20 +0800

    Fix find_mean_and_stddev test

--- tests/util.py
+++ tests/util.py
@@ -1,4 +1,7 @@
+import numpy as np
+import torch
 from torchvision.datasets import CIFAR10
+from torchvision.transforms import v2
 
 from hw2 import PROJECT_ROOT
 from hw2.util import find_mean_and_stddev, CIFAR10_NORMALIZATION
@@ -6,5 +9,9 @@ from hw2.util import find_mean_and_stddev, CIFAR10_NORMALIZATION
 
 def test_cifar_10_normalization() -> None:
     """Test that the CIFAR-10 normalization is correct."""
-    cifar10_normalization = find_mean_and_stddev(CIFAR10(root=PROJECT_ROOT / "data", train=True, transform=None))
-    assert cifar10_normalization == CIFAR10_NORMALIZATION
+    transform = v2.Compose([
+        v2.ToImage(),
+        v2.ToDtype(torch.float32, scale=True),
+    ])
+    cifar10_normalization = find_mean_and_stddev(CIFAR10(root=PROJECT_ROOT / "data", train=True, transform=transform))
+    assert np.allclose(cifar10_normalization, CIFAR10_NORMALIZATION, atol=1e-3)
