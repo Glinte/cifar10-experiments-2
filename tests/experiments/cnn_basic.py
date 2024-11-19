ea0261537024cafad1f1ded91158cd823f6b32b5
Glinte <96855131+Glinte@users.noreply.github.com>
2024-11-19 09:21:07 +0800

    Add testing to CNN

---
+++ tests/experiments/cnn_basic.py
@@ -0,0 +1,32 @@
+import pytest
+
+from hw2.models.cnn_basic import CNN
+from hw2.util import find_max_batch_size
+
+
+@pytest.mark.hardware_dependent
+def test_maximum_batch_size():
+    model = CNN(channels=3)
+    input_size = (3, 32, 32)
+    device = "cuda"
+    start_batch_size = 1
+    max_batch_size = find_max_batch_size(model, input_size, device, start_batch_size)
+    print(f"Maximum batch size: {max_batch_size}")
+    assert max_batch_size == 65536
+
+
+def test_varying_batch_size_on_training_performance():
+    model = CNN(channels=3)
+    raise NotImplementedError("This test is not implemented yet.")
+
+def test_varying_learning_rate_on_training_performance():
+    model = CNN(channels=3)
+    raise NotImplementedError("This test is not implemented yet.")
+
+def test_different_optimizers():
+    model = CNN(channels=3)
+    raise NotImplementedError("This test is not implemented yet.")
+
+def test_epochs():
+    model = CNN(channels=3)
+    raise NotImplementedError("This test is not implemented yet.")
