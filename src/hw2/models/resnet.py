6786d12cfadee0f64f4860c72ac5ed2b22a29401
Glinte <96855131+Glinte@users.noreply.github.com>
2024-11-22 21:39:43 +0800

    Finish ResNet training

--- src/hw2/models/resnet.py
+++ src/hw2/models/resnet.py
@@ -1,34 +1,24 @@
+import logging
+
 import torch
 import torchvision
 import wandb
 from torch import nn
 from torchvision.transforms import v2
 
+from hw2 import PROJECT_ROOT
 from hw2.open_set import one_minus_max_of_softmax
 from hw2.util import CIFAR10_NORMALIZATION, train_on_cifar10
 
 
-def validate(model, test_loader, criterion):
-    model.eval()
-    running_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for images, labels in test_loader:
-            outputs = model(images)
-            running_loss += criterion(outputs, labels).item()
-            _, predicted = torch.max(outputs, 1)
-            correct += (predicted == labels).sum().item()
-    running_loss /= len(test_loader)
-    accuracy = correct / len(test_loader.dataset)
-    return running_loss, accuracy
-
-
 def main():
+    logging.basicConfig(level=logging.INFO)
     epochs = 10
     learning_rate = 0.001
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
-    model = torchvision.models.resnet18()
+    model = torchvision.models.resnet50()
+    # model = torchvision.models.resnet18()
     model.fc = nn.Linear(in_features=model.fc.in_features, out_features=10)
 
     transforms = v2.Compose([
@@ -45,7 +35,7 @@ def main():
         project="CISC3027 hw2",
         job_type="train",
         config={
-            "model": "ResNet18",
+            "model": "ResNet50",  # Remember to change this to ResNet18 if you are using ResNet18
             "epochs": epochs,
             "batch_size": 64,
             "learning_rate": learning_rate,
@@ -53,7 +43,12 @@ def main():
         }
     )
 
-    train_on_cifar10(model, optimizer, criterion, transforms, epochs, device=device, open_set_prob_fn=open_set_prob_fn)
+    loss, acc = train_on_cifar10(model, optimizer, criterion, transforms, epochs, device=device, open_set_prob_fn=open_set_prob_fn, log_run=True)
+    torch.save(model.state_dict(), PROJECT_ROOT / f"models/resnet500_epoch{epochs}_{acc:.4f}.pth")
+
+    # model.load_state_dict(torch.load(PROJECT_ROOT / "models/resnet18_epoch10_0.7329.pth"))
+    # results = validate_on_cifar10(model, criterion, transforms, device, additional_metrics=[partial(metrics.classification_report, digits=4)])
+    # print(results["classification_report"])
 
     run.finish()
 
