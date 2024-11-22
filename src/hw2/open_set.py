1426a03b51ba21563a1cb7ce02ba652822ed230d
Glinte <96855131+Glinte@users.noreply.github.com>
2024-11-22 18:05:36 +0800

    Update entropy_of_softmax to have n_classes defaults = 10
    Helpful signatures

--- src/hw2/open_set.py
+++ src/hw2/open_set.py
@@ -7,7 +7,7 @@ def one_minus_max_of_softmax(x: torch.Tensor) -> torch.Tensor:
     return x.softmax(dim=1).max(dim=1).values
 
 
-def entropy_of_softmax(x: torch.Tensor, n_classes: int) -> torch.Tensor:
+def entropy_of_softmax(x: torch.Tensor, n_classes: int = 10) -> torch.Tensor:
     """Compute the entropy of the softmax probabilities, normalized by the maximum possible entropy."""
     max_entropy = torch.log(torch.tensor(n_classes).float())
     print(max_entropy)
