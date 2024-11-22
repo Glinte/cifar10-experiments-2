fabbf002f532395e8ae6b36d62870e5c1bfe93cb
Glinte <96855131+Glinte@users.noreply.github.com>
2024-11-22 18:06:40 +0800

    Remove debug print

--- src/hw2/open_set.py
+++ src/hw2/open_set.py
@@ -10,6 +10,5 @@ def one_minus_max_of_softmax(x: torch.Tensor) -> torch.Tensor:
 def entropy_of_softmax(x: torch.Tensor, n_classes: int = 10) -> torch.Tensor:
     """Compute the entropy of the softmax probabilities, normalized by the maximum possible entropy."""
     max_entropy = torch.log(torch.tensor(n_classes).float())
-    print(max_entropy)
     entropy = -(x.softmax(dim=1) * x.log_softmax(dim=1)).sum(dim=1)
     return entropy / max_entropy
