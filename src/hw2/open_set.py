b9dfb6fc4a1d1acb2fba2cb8f41c4c7ee6c082ed
Glinte <96855131+Glinte@users.noreply.github.com>
2024-11-24 08:50:19 +0800

    Fix the open set functions
    They are completely wrong. oops.

--- src/hw2/open_set.py
+++ src/hw2/open_set.py
@@ -2,13 +2,13 @@
 import torch
 
 
-def one_minus_max_of_softmax(x: torch.Tensor) -> torch.Tensor:
-    """Compute one minus the maximum of the softmax probabilities."""
-    return x.softmax(dim=1).max(dim=1).values
+def one_minus_max_of_prob(x: torch.Tensor) -> torch.Tensor:
+    """Compute one minus the maximum of the class probabilities."""
+    return 1 - x.max(dim=1).values
 
 
 def entropy_of_softmax(x: torch.Tensor, n_classes: int = 10) -> torch.Tensor:
-    """Compute the entropy of the softmax probabilities, normalized by the maximum possible entropy."""
+    """Compute the entropy of the class probabilities, normalized by the maximum possible entropy."""
     max_entropy = torch.log(torch.tensor(n_classes).float())
-    entropy = -(x.softmax(dim=1) * x.log_softmax(dim=1)).sum(dim=1)
+    entropy = -torch.sum(x * torch.log(x), dim=1)
     return entropy / max_entropy
