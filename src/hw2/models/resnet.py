2a3d2461de9c350506e6db64b9494a9bb1c81762
Glinte <96855131+Glinte@users.noreply.github.com>
2024-11-24 21:37:33 +0800

    Update resnet.py

--- src/hw2/models/resnet.py
+++ src/hw2/models/resnet.py
@@ -1,14 +1,16 @@
 import logging
+from functools import partial
 
 import torch
 import torchvision
 import wandb
+from sklearn import metrics
 from torch import nn
 from torchvision.transforms import v2
 
 from hw2 import PROJECT_ROOT
-from hw2.open_set import one_minus_max_of_softmax
-from hw2.util import CIFAR10_NORMALIZATION, train_on_cifar10
+from hw2.open_set import one_minus_max_of_prob
+from hw2.util import CIFAR10_NORMALIZATION, train_on_cifar10, validate_on_cifar10
 
 
 def main():
@@ -29,28 +31,28 @@ def main():
 
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
-    open_set_prob_fn = one_minus_max_of_softmax
+    open_set_prob_fn = one_minus_max_of_prob
 
-    run = wandb.init(
-        project="CISC3027 hw2",
-        job_type="train",
-        config={
-            "model": "ResNet50",  # Remember to change this to ResNet18 if you are using ResNet18
-            "epochs": epochs,
-            "batch_size": 64,
-            "learning_rate": learning_rate,
-            "optimizer": optimizer.__class__.__name__,
-        }
-    )
+    # run = wandb.init(
+    #     project="CISC3027 hw2",
+    #     job_type="train",
+    #     config={
+    #         "model": "ResNet50",  # Remember to change this to ResNet18 if you are using ResNet18
+    #         "epochs": epochs,
+    #         "batch_size": 64,
+    #         "learning_rate": learning_rate,
+    #         "optimizer": optimizer.__class__.__name__,
+    #     }
+    # )
 
-    loss, acc = train_on_cifar10(model, optimizer, criterion, transforms, epochs, device=device, open_set_prob_fn=open_set_prob_fn, log_run=True)
-    torch.save(model.state_dict(), PROJECT_ROOT / f"models/resnet500_epoch{epochs}_{acc:.4f}.pth")
+    # loss, acc = train_on_cifar10(model, optimizer, criterion, transforms, epochs, device=device, open_set_prob_fn=open_set_prob_fn, log_run=True)
+    # torch.save(model.state_dict(), PROJECT_ROOT / f"models/resnet50_epoch{epochs}_{acc:.4f}.pth")
 
-    # model.load_state_dict(torch.load(PROJECT_ROOT / "models/resnet18_epoch10_0.7329.pth"))
-    # results = validate_on_cifar10(model, criterion, transforms, device, additional_metrics=[partial(metrics.classification_report, digits=4)])
-    # print(results["classification_report"])
+    model.load_state_dict(torch.load(PROJECT_ROOT / "models/resnet50_epoch10_0.7344.pth"))
+    results = validate_on_cifar10(model, criterion, transforms, device, additional_metrics=[partial(metrics.classification_report, digits=4)])
+    print(results["classification_report"])
 
-    run.finish()
+    # run.finish()
 
 if __name__ == "__main__":
     main()
