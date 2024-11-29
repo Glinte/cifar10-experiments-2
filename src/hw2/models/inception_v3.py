1daa3c3ff54a4c246d92dc6100331bdc99d0b401
Glinte <96855131+Glinte@users.noreply.github.com>
2024-11-29 17:22:21 +0800

    Updates

--- src/hw2/models/inception_v3.py
+++ src/hw2/models/inception_v3.py
@@ -25,3 +25,65 @@ def get_inception_v3_transform() -> Callable:
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
+
+
+if __name__ == '__main__':
+    # Test the model and transform
+    model = get_inception_v3(num_classes=10)
+    transform = get_inception_v3_transform()
+    print(model)
+    print(transform)
+    print("Done!")
+
+    config = {
+        "model_type": "CNN",
+        "_model_auto": model.__class__.__name__,
+        "model": "Inception v3",
+        "epochs": 40,
+        "learning_rate": 0.001,
+        "batch_size": 64,
+        "device": torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu"),
+        "dataset": "CIFAR100LT"
+    }
+
+    tags = ["weighted"]
+
+    if config["dataset"] == "CIFAR100LT":
+        class_weights = 1 / torch.tensor(dataset.img_num_per_cls, device=config["device"])
+        sample_weights = [class_weights[target] for target in dataset.targets]
+        sampler = BatchSampler(
+            WeightedRandomSampler(weights=sample_weights, num_samples=len(dataset), replacement=True), batch_size=64,
+            drop_last=False)
+    else:
+        class_weights = None
+        sampler = None
+
+    criterion = nn.CrossEntropyLoss(weight=class_weights)
+    optimizer = optim.AdamW(model.parameters(), weight_decay=0.1, lr=config["learning_rate"])
+    scheduler = lr_scheduler.CosineAnnealingLR(optimizer, T_max=config["epochs"], eta_min=0)
+
+    config.update({
+        "optimizer": optimizer.__class__.__name__,
+        "lr_scheduler": scheduler.__class__.__name__,
+    })
+
+    run = wandb.init(
+        project="CISC3027 hw3",
+        group=None,
+        job_type="train",
+        config=config,
+        dir=PROJECT_ROOT / "wandb",
+        tags=tags,
+    )
+
+    logging.basicConfig(level=logging.INFO, force=True)
+
+    # train
+    train_on_cifar(model, optimizer, criterion, scheduler, transform, config["epochs"], config["device"], log_run=True,
+                   cifar_dataset=config["dataset"], n_test_samples=10000, batch_sampler=sampler)
+
+    results = validate_on_cifar(model, criterion, CIFAR100LT.basic_test_transform, config["device"],
+                                additional_metrics=[partial(metrics.classification_report, digits=4, zero_division=0)],
+                                cifar_dataset="CIFAR100LT")
+
+    torch.save(model.state_dict(), PROJECT_ROOT / "models" / "cifar100lt" / f"inceptionv3_{results["accuracy"]}.pth")
